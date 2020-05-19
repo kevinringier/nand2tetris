486e7826bf7be8a2981406e6af3dbfd38f8ade6a
kevin ringier <kevinringier@gmail.com>
2020-05-19 08:42:24 -0600

    complete chapter 4 exercises

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,86 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// initialize color state
+@colored
+M=0
+
+(LOOP)
+// check keyboard input
+@24576
+D=M
+
+@COLORSCREEN
+D;JGT
+
+@ERASESCREEN
+D;JEQ
+
+(COLORSCREEN)
+// if colored, goto loop
+@colored
+D=M
+@LOOP
+D;JGT
+
+@i
+M=0
+
+// if not, color screen
+(COLORLOOP)
+@i
+D=M
+@16384
+A=A+D
+M=-1
+
+@i
+M=M+1
+D=M
+@16384
+D=D+A
+@24576
+D=A-D
+@COLORLOOP
+D;JGT
+
+@colored
+M=1
+
+@LOOP
+0;JMP
+
+(ERASESCREEN)
+// if not colored, goto loop
+@colored
+D=M
+@LOOP
+D;JEQ
+
+@i
+M=0
+
+// if not, color screen
+(ERASELOOP)
+@i
+D=M
+@16384
+A=A+D
+M=0
+
+@i
+M=M+1
+D=M
+@16384
+D=D+A
+@24576
+D=A-D
+@ERASELOOP
+D;JGT
+
+@colored
+M=0
+
+@LOOP
+0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,33 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@i 
+M=0 // i = 0
+@product
+M=0 // product = 0
+
+(LOOP)
+    @i
+    D=M // D = i
+    @1
+    D=M-D // D = R1 - i 
+    @END
+    D;JLE // if D <= 0 goto END
+    
+    @i
+    M=M+1 // i = i + 1
+    @0
+    D=M // D = R0
+    @product
+    M=D+M // product = product + R0
+
+    @LOOP
+    0;JMP // goto LOOP
+
+(END)
+
+@product
+D=M // D = product
+
+@2
+M=D // R2 = D
